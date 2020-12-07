3cc2eec1dac645ce4aebd413b691e1b6a41bdda6
MenvielleValen <70424829+MenvielleValen@users.noreply.github.com>
2020-12-07 20:50:06 -0300

    sds
    nueva comit

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,12 @@
 const harmburger=document.querySelector(".hamburger");
 const menu=document.querySelector(".menu-navegacion");
 
+const instagram= document.querySelector(".instagram");
+const facebook= document.querySelector(".facebook");
+const wsp= document.querySelector(".wsp");
+
+
+
 harmburger.addEventListener('click', ()=>{
     menu.classList.toggle("spread");
 })
